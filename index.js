cc9d702bcf323ee4f76080b29751fe165d2c37fd
unverzed <vitoriarortega@gmail.com>
2022-06-09 16:05:41 -0300

    feat: connection to database

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRouter from "./routes/authRouter.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 dotenv.config();
+app.use(authRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server working on port ${process.env.PORT}`));
