e458c43182186e619c691e49a5e3dcbb42983f6a
unverzed <vitoriarortega@gmail.com>
2022-06-10 15:12:42 -0300

    feat: get url by id

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -15,7 +15,28 @@ export async function shortenUrl(req, res) {
     res.status(201).send({ shortUrl: shortUrl });
   } catch (e) {
     console.log("erro ao encurtar", { userId });
-    res.status(500).send("Erro ao encurtar url");
+    res.sendStatus(500);
   }
 }
 
+export async function getURL(req, res) {
+  const { id } = req.params;
+
+  try {
+    const result = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE "userId" = $1`, [
+      id,
+    ]);
+
+    const url = result.rows[0];
+
+    if (!result.rows[0]) {
+      return res.sendStatus(404);
+    }
+  
+    res.status(200).send(url);
+
+  } catch (e) {
+    console.log("erro ao pegar a url do id", id);
+    res.sendStatus(500);
+  }
+}
--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/urlController.js";
+import { shortenUrl, getURL } from "../controllers/urlController.js";
 import { validateToken } from "../middlewares/tokenMiddleware.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validateToken, shortenUrl);
+urlRouter.get("/urls/:id", getURL);
 
 export default urlRouter;
