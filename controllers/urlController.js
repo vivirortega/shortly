d59412722d1803c3077d6985d44a796c4ec52516
unverzed <vitoriarortega@gmail.com>
2022-06-10 16:09:51 -0300

    feat: get openUrl

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -23,20 +23,47 @@ export async function getURL(req, res) {
   const { id } = req.params;
 
   try {
-    const result = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE "userId" = $1`, [
-      id,
-    ]);
+    const result = await db.query(
+      `SELECT id, "shortUrl", url FROM urls WHERE "userId" = $1`,
+      [id]
+    );
 
     const url = result.rows[0];
 
     if (!result.rows[0]) {
       return res.sendStatus(404);
     }
-  
-    res.status(200).send(url);
 
+    res.status(200).send(url);
   } catch (e) {
     console.log("erro ao pegar a url do id", id);
+    res.sendStatus(500);
+  }
+}
+
+export async function openURL(req, res) {
+  const { shortUrl } = req.params;
+  try {
+    const result = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1`, [
+      shortUrl,
+    ]);
+
+    if (!result.rows[0]) {
+      return res.sendStatus(404);
+    }
+    const url = result.rows[0].url;
+
+    const updateVisits = await db.query(
+      `UPDATE urls
+      SET visits = visits + 1
+      WHERE "shortUrl" = $1
+      `,
+      [shortUrl]
+    );
+
+    res.redirect(url);
+  } catch (e) {
+    console.log("erro ao abrir a url", e);
     res.sendStatus(500);
   }
 }
--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { shortenUrl, getURL } from "../controllers/urlController.js";
+import { shortenUrl, getURL, openURL } from "../controllers/urlController.js";
 import { validateToken } from "../middlewares/tokenMiddleware.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validateToken, shortenUrl);
 urlRouter.get("/urls/:id", getURL);
+urlRouter.get("/urls/open/:shortUrl", openURL);
 
 export default urlRouter;
