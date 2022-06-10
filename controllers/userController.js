e05dbd315e921011744467d36842d5c5aa8b7a43
unverzed <vitoriarortega@gmail.com>
2022-06-10 19:22:23 -0300

    feat: get ranking

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import db from "../database/db.js";
 
 export async function getUser(req, res) {
   const { id } = req.params;
-  
+
   try {
     const result = await db.query(
       `SELECT users.id, users.name, SUM(COALESCE(urls.visits, 0)) AS "visitCount" FROM users
@@ -16,16 +16,34 @@ export async function getUser(req, res) {
       return res.sendStatus(404);
     }
 
-    const users = result.rows[0];
+    const users = result.rows;
 
     const shortUrls = await db.query(
       `SELECT id, "shortUrl", url, visits AS "visitCount" FROM urls WHERE "userId"=$1`,
       [id]
     );
 
-    const shortenedUrls = shortUrls.rows[0];
+    const shortenedUrls = shortUrls.rows;
 
     res.status(200).send({ ...users, shortenedUrls });
+  } catch (e) {
+    console.log("erro ao pegar os usuários", e);
+    res.sendStatus(500);
+  }
+}
+
+export async function getRanking(req, res) {
+  try {
+    const result =
+      await db.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(COALESCE(urls.visits, 0)) AS "visitCount"
+        FROM users
+        LEFT JOIN urls ON users.id = urls."userId"
+        GROUP BY users.id
+        ORDER BY "visitCount" DESC
+        LIMIT 10;`);
+
+    const ranking = result.rows;
+    res.status(200).send(ranking);
   } catch (e) {
     console.log("erro ao pegar os usuários", e);
     res.sendStatus(500);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUser } from "../controllers/userController.js";
+import { getUser, getRanking } from "../controllers/userController.js";
 import { validateToken } from "../middlewares/tokenMiddleware.js"
 
 const userRouter = Router();
 
 userRouter.get("/users/:id", validateToken, getUser);
+userRouter.get("/ranking", getRanking);
 
 export default userRouter;
